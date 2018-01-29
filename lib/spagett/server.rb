cbb05e022fac6aa774ee5c5c65cd9dfe77c020b6
Neil Johari <neil@johari.tech>
2018-01-28 22:01:06 -0500

    Stub out thread updates

--- lib/spagett/server.rb
+++ lib/spagett/server.rb
@@ -12,13 +12,20 @@ module Spagett
         puts "Successfully connected, welcome '#{client.self.name}' to the '#{client.team.name}' team at https://#{client.team.domain}.slack.com."
       end
 
+      settings.client.on :message do |data|
+        next unless data.message.try(:thread_ts)
+        next unless data.channel == ENV["SUPPORT_CHANNEL"]
+
+        puts "Updating syosseths.com with reply to thread #{data.message.thread_ts} with #{data.message.text}"
+      end
+
       settings.client.start_async
 
       super
     end
 
     post '/thread' do # params: id, user
-      puts settings.client.web_client.chat_postMessage channel: "#support", text: "Hi!"
+      puts settings.client.web_client.chat_postMessage channel: ENV["SUPPORT_CHANNEL"], text: "Thread reply/creation incoming from syosseths.com"
     end
   end
 end
